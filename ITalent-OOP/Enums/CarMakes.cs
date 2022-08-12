5d2d4491f0d831adb0c609d91e614fea81d42951
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-13 00:42:59 +0300

    Enums Added

---
+++ ITalent-OOP/Enums/CarMakes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITalent_OOP.Enums
+{
+    internal enum CarMakes
+    {
+        Fiat,
+        Lancia,
+        Citroen,
+        Mercedes,
+        Audi
+    }
+}
