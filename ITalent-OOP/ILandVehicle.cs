0fbb0015740ea6174ee69f6985c3431c697888e0
Serkan SAHIN <serkansahin114@gmail.com>
2022-08-15 02:28:17 +0300

    Vehicle ve ILandVehicleInterfacesi eklendi

---
+++ ITalent-OOP/ILandVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITalent_OOP
+{
+    internal interface ILandVehicle
+    {
+        public int shift { get; set; }
+        void Horn();
+        void Shift(int shiftValue);
+    }
+}
---
+++ ITalent-OOP/Vehicle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ITalent_OOP.Enums;
+
+namespace ITalent_OOP
+{
+    internal class Vehicle
+    {
+        private int _year;
+        private int _horsePower;
+        public FuelTypes fuelType;
+        public int speed;
+
+        public event Action<int> yearEventOuterBounds;
+        public event Action<int> yearEventValueChanged;
+        public int year 
+        {
+            get { return _year; }
+            set 
+            {
+                if (value < 2000 || value > 2022)
+                {
+                    if (yearEventOuterBounds != null)
+                        yearEventOuterBounds(value);
+                }
+                else
+                {
+                    if (yearEventValueChanged != null)
+                        yearEventValueChanged(value);
+                    _year = value;
+                }
+            }
+        }
+
+        public int horsepower
+        {
+            get { return _horsePower; }
+            set
+            {
+                if (value < 0)
+                    return;
+                else
+                    _horsePower = value;
+            }
+        }
+
+        public Vehicle(){}
+        public Vehicle(int year)
+        {
+            _year = year;
+        }
+
+        public Vehicle(int year, int horsePower) : this(year)
+        {
+            _horsePower = horsePower;
+        }
+
+        public Vehicle(int year, int horsePower, FuelTypes fuelType) : this(year,horsePower)
+        {
+            this.fuelType = fuelType;
+        }
+
+        public virtual void SpeedIncrease(int value)
+        {
+            speed = value;
+            Console.WriteLine("Hız Arttırıldı");
+        }
+
+        
+
+    }
+}
